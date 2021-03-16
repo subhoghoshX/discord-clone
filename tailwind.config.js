49c1f3b5985ebf08c4ab57c79b922849cd852c2f
Subho <contact.subhoghosh@gmail.com>
2021-03-16 17:20:44 +0530

    update(tailwind config): enable all colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: colors,
     extend: {},
   },
   variants: {
