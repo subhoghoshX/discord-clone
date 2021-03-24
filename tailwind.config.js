b98cf470cd3e90e5363a50f7abdcb783753da71e
Subho <contact.subhoghosh@gmail.com>
2021-03-24 20:04:28 +0530

    Update(tailwind.config.js): use shorthand syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}', './src/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: colors,
+    colors,
     extend: {
       transitionProperty: {
         rounded: 'border-radius',
